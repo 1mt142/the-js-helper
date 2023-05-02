3a46ca6106af7aa2944a3f9eebf02af606ae1d1b
Imtiaz Khandoker <imtiazkhandoker@Imtiazs-MacBook.local>
2023-05-02 23:12:41 +0600

    fix issues

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,26 @@
 "use strict";
+/**
+ * Builds a query string from an object.
+ * @param data {object}
+ * @returns {string}
+ */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.buildQueryString = void 0;
 var buildQueryString = function (data) {
     var query = "";
-    var first = true;
+    var isFirst = true;
     for (var _i = 0, _a = Object.entries(data); _i < _a.length; _i++) {
         var _b = _a[_i], key = _b[0], value = _b[1];
         if (typeof value === "object" && value !== null) {
             var subQuery = (0, exports.buildQueryString)(value);
             if (subQuery !== "") {
-                query += "".concat(first ? "" : "&").concat(subQuery);
-                first = false;
+                query += "".concat(isFirst ? "" : "&").concat(subQuery);
+                isFirst = false;
             }
         }
         else if (value !== null && value !== undefined) {
-            query += "".concat(first ? "?" : "&").concat(key, "=").concat(encodeURIComponent(value));
-            first = false;
+            query += "".concat(isFirst ? "?" : "&").concat(key, "=").concat(encodeURIComponent(value));
+            isFirst = false;
         }
     }
     return query;
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
-export const buildQueryString = (data: { [key: string]: any }): string => {
+/**
+ * Builds a query string from an object.
+ * @param data {object}
+ * @returns {string}
+ */
+
+export const buildQueryString = (data: Record<string, any>): string => {
   let query = "";
-  let first = true;
+  let isFirst = true;
   for (const [key, value] of Object.entries(data)) {
     if (typeof value === "object" && value !== null) {
       const subQuery = buildQueryString(value);
       if (subQuery !== "") {
-        query += `${first ? "" : "&"}${subQuery}`;
-        first = false;
+        query += `${isFirst ? "" : "&"}${subQuery}`;
+        isFirst = false;
       }
     } else if (value !== null && value !== undefined) {
-      query += `${first ? "?" : "&"}${key}=${encodeURIComponent(value)}`;
-      first = false;
+      query += `${isFirst ? "?" : "&"}${key}=${encodeURIComponent(value)}`;
+      isFirst = false;
     }
   }
   return query;
