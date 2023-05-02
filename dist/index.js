d32e7c64946d445ae5b78a0cdc15c98bd57b592f
Imtiaz Khandoker <imtiazkhandoker@Imtiazs-MacBook.local>
2023-05-03 01:20:58 +0600

    fix issues

--- dist/index.js
+++ dist/index.js
@@ -4,13 +4,15 @@
  * @param data {Record<string, any>} can be object or nested object
  * @returns {string}
  */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.buildQueryString = void 0;
 var buildQueryString = function (data) {
     var query = "";
     var first = true;
     for (var _i = 0, _a = Object.entries(data); _i < _a.length; _i++) {
         var _b = _a[_i], key = _b[0], value = _b[1];
         if (typeof value === "object" && value !== null) {
-            var subQuery = buildQueryString(value);
+            var subQuery = (0, exports.buildQueryString)(value);
             if (subQuery !== "") {
                 query += "".concat(first ? "" : "&").concat(subQuery);
                 first = false;
@@ -23,3 +25,4 @@ var buildQueryString = function (data) {
     }
     return query;
 };
+exports.buildQueryString = buildQueryString;
