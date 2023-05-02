1dd18049483d8e32a2692ad5cf9f518ee05c3119
Imtiaz Khandoker <imtiazkhandoker@Imtiazs-MacBook.local>
2023-05-03 01:09:17 +0600

    fix issues

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,25 @@
 "use strict";
 /**
  * Builds a query string from an object.
- * @param data {object}
+ * @param data {Record<string, any>} can be object or nested object
  * @returns {string}
  */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.buildQueryString = void 0;
 var buildQueryString = function (data) {
     var query = "";
-    var isFirst = true;
+    var first = true;
     for (var _i = 0, _a = Object.entries(data); _i < _a.length; _i++) {
         var _b = _a[_i], key = _b[0], value = _b[1];
         if (typeof value === "object" && value !== null) {
-            var subQuery = (0, exports.buildQueryString)(value);
+            var subQuery = buildQueryString(value);
             if (subQuery !== "") {
-                query += "".concat(isFirst ? "" : "&").concat(subQuery);
-                isFirst = false;
+                query += "".concat(first ? "" : "&").concat(subQuery);
+                first = false;
             }
         }
         else if (value !== null && value !== undefined) {
-            query += "".concat(isFirst ? "?" : "&").concat(key, "=").concat(encodeURIComponent(value));
-            isFirst = false;
+            query += "".concat(first ? "" : "&").concat(key, "=").concat(encodeURIComponent(value));
+            first = false;
         }
     }
     return query;
 };
-exports.buildQueryString = buildQueryString;
